9ac2c25908b335d4a22ebb3496f976d3268f6ac0
Giovanni Giacobbi <giovanni@giacobbi.net>
2022-10-04 23:34:31 +0200

    tail_http_error_log: Same updates of the access log common parts

--- scripts/tail_http_error_log.php
+++ scripts/tail_http_error_log.php
@@ -1,5 +1,10 @@
 #!/usr/bin/php
-<?php
+<?php declare(ticks=1);
+/**
+ * tail_http_error_log
+ *
+ * ...
+ */
 
 define("THSYSTOOLS_SHARED_PATH", "@@THSYSTOOLS_SHARED_PATH@@");
 
@@ -9,82 +14,211 @@ else
   require __DIR__ . DIRECTORY_SEPARATOR . "wtools.inc.php";
 
 /**
- * ...
+ * Prints an error message and terminates the execution
+ *
+ * @param string $message Error message
+ * @return never
  */
-class TestTail {
-  public function __construct() {
-    $this->_testcases = array(
-      '127.0.0.1 - - [11/Nov/2017:21:43:43 +0100] "POST /g\\0et\\"cfg.php HTTP/1.1" 200 1 "\\"xx\\"" "AG\\"ENT"',
-      '127.0.0.1 - - [11/Nov/2017:21:43:43 +0100] "POST /normal.php HTTP/1.1" 200 1 "-" "AGENT"'
-    );
-  }
-  public function read() {
-    $retval = array_shift($this->_testcases);
-    if ($retval === null)
-      return false;
-    return $retval;
-  }
+function err($message)
+{
+  fprintf(STDERR, "Error: %s\n", $message);
+  exit(1);
 }
 
 /**
- * ...
+ * Prints a debugging message to the standard error stream
  *
- * @param string $message ...
+ * @param string $message Debug message
+ * @return void
  */
-function dbg($message) {
-  // print "[d] " . $message . "\n";
+function dbg($message)
+{
+  global $opt_debug;
+  if ($opt_debug)
+    fprintf(STDERR, "[d] %s\n", $message);
+}
+
+/**
+ * Prints the command usage information on the given stream
+ *
+ * @param resource $fd Stream for output
+ * @param string $progname Command line name
+ * @return void
+ */
+function print_usage($fd, $progname)
+{
+  fprintf($fd, "Usage: %s [-c COLS] [-S|-B SIZE] [-f] <file> [filters...]\n", $progname);
+}
+
+/**
+ * System function to get the current terminal's columns available
+ *
+ * @return int Number of columns available
+ */
+function sys_get_term_cols()
+{
+  $cols = (int) exec("tput cols 2>/dev/null");
+  if (!$cols)
+    return 80;
+
+  return $cols;
 }
 
 /* ------------------------------------------------------------------------ */
 
-$opt_lines = 5;
-$opt_test = false;
+$opt_help = false;
+$opt_debug = false;
 $opt_cols = null;
-$argidx = 1;
+$opt_rollback_bytes = 32768;
+$opt_rollback_lines = 5;
+$opt_follow = false;
+$opt_filter = array();
+$files = array();
 
-/* read command line switch "-n LINES" */
-if (isset($argv[$argidx]) && ($argv[$argidx] == "-n")) {
-  $opt_lines = (int) (isset($argv[$argidx + 1]) ? $argv[$argidx + 1] : null);
-  $argidx += 2;
+$local_args = $argv;
+$progname = array_shift($local_args);
+
+/* read command line switch "-h" */
+while (($k = array_search("-h", $local_args)) !== false) {
+  $opt_help = true;
+  array_splice($local_args, $k, 1);
+}
+
+/* read command line switch "-d" */
+while (($k = array_search("-d", $local_args)) !== false) {
+  $opt_debug = true;
+  array_splice($local_args, $k, 1);
 }
 
 /* read command line switch "-c COLS" */
-if (isset($argv[$argidx]) && ($argv[$argidx] == "-c")) {
-  $opt_cols = (int) (isset($argv[$argidx + 1]) ? $argv[$argidx + 1] : null);
-  $argidx += 2;
+while (($k = array_search("-c", $local_args)) !== false) {
+  $opt_cols = (int) (isset($local_args[$k + 1]) ? $local_args[$k + 1] : null);
+  array_splice($local_args, $k, 2);
 }
 
-/* read command line switch "-test" */
-if (isset($argv[$argidx]) && ($argv[$argidx] == "-test")) {
-  $opt_test = true;
-  $argidx++;
+/* read command line switch "-S" */
+while (($k = array_search("-S", $local_args)) !== false) {
+  $opt_rollback_bytes = true;
+  array_splice($local_args, $k, 1);
 }
+
+/* read command line switch "-B SIZE" */
+while (($k = array_search("-B", $local_args)) !== false) {
+  $_arg = (string) (isset($local_args[$k + 1]) ? $local_args[$k + 1] : null);
+  if (!preg_match('/^(\d+)(M|k)?$/', $_arg, $regp))
+    err("Invalid syntax \"$_arg\" for filter, must be a bytes unit");
+  $opt_rollback_bytes = (int) $regp[1];
+  if (!empty($regp[2])) {
+    $opt_rollback_bytes *= ($regp[2] == "M" ? 1024 * 1024 :
+        ($regp[2] == "k" ? 1024 : 1));
+  }
+  array_splice($local_args, $k, 2);
+}
+
+/* read command line switch "-n LINES" */
+while (($k = array_search("-n", $local_args)) !== false) {
+  $opt_rollback_lines = (int) (isset($local_args[$k + 1]) ? $local_args[$k + 1] : null);
+  array_splice($local_args, $k, 2);
+}
+
+/* read command line switch "-f" */
+while (($k = array_search("-f", $local_args)) !== false) {
+  $opt_follow = true;
+  array_splice($local_args, $k, 1);
+}
+
+if ($opt_help) {
+  print_usage(STDOUT, $progname);
+  exit(0);
+}
+
+/* process remaining argument, depending on the type, bucket-sort them */
+$_use_only_files = false;
+foreach ($local_args as $_arg) {
+  /* after '--', only files are encountered */
+  if ($_use_only_files) {
+    $files[] = $_arg;
+  }
+  elseif ($_arg == "--") {
+    $_use_only_files = true;
+  }
+  elseif ($_arg == "-") {
+    $files[] = "php://stdin";
+  }
+  elseif (substr($_arg, 0, 1) == "-") {
+    err("Unknown option '$_arg'");
+  }
+  elseif (strpos($_arg, "=") !== false) {
+    if (!preg_match('/^([a-z]+)=(.+)$/', $_arg, $regp))
+      err("Invalid syntax \"$_arg\" for filter, must be key=value");
+    $opt_filter[] = array($regp[1],
+        preg_split('/,/', $regp[2], PREG_SPLIT_NO_EMPTY));
+  }
+  else {
+    $files[] = $_arg;
+  }
+}
+
+dbg("Runtime options:");
+dbg("..  opt_cols = " . ($opt_cols !== null ? $opt_cols : "(auto)"));
+dbg("..  opt_rollback_lines = " . $opt_rollback_lines);
+dbg("..  opt_rollback_bytes = " . $opt_rollback_bytes);
+dbg("..  opt_follow = " . ($opt_follow ? "true" : "false"));
+dbg("..  opt_filter = " . json_encode($opt_filter));
 
 /* ------------------------------------------------------------------------ */
 
 /* auto-determine the number of usable columns */
 if ($opt_cols === null) {
-  dbg("trying to auto-determine total number of columns");
-  $opt_cols = (int) exec("tput cols");
-  dbg("detected screen columns: " . $opt_cols);
+  dbg("Trying to auto-determine total number of columns");
+  $opt_cols = sys_get_term_cols();
+  dbg(".. detected screen columns: " . $opt_cols);
+
+  if (function_exists('pcntl_signal')) {
+    dbg("Setting up terminal resize signal handler (SIGWINCH)");
+    pcntl_signal(SIGWINCH, function($signo, $siginfo) {
+      global $opt_cols;
+      dbg("Terminal resized, attempting to detect new number of columns");
+      $opt_cols = sys_get_term_cols();
+      dbg(".. detected screen columns: " . $opt_cols);
+    });
+  }
 }
 
-/* execute tests now if requested */
-if ($opt_test) {
-  $tail = new TestTail($file);
+/* read first command argument (file name) */
+$file = null;
+if (count($files) > 0)
+  $file = array_shift($files);
+if ($file == "-") {
+  $file = "php://stdin";
+}
+elseif ($file == "") {
+  if ($file === null) {
+    /* attempt to find the most obvious file */
+    $CandidateFiles = array(
+      "%HOME/logs/error_log",
+      "./error_log");
+
+    $repls = array(array("%HOME"), array(getenv("HOME")));
+    foreach ($CandidateFiles as $candidate_file) {
+      $candidate_file = str_replace($repls[0], $repls[1], $candidate_file);
+      if (file_exists($candidate_file))
+        $file = $candidate_file;
+    }
+  }
+
+  if ($file == "") {
+    print_usage(STDERR, $progname);
+    exit(1);
+  }
+}
+
+$tail = new Tail($file, $opt_follow);
+if ($opt_rollback_bytes === true) {
+  $tail->rollback();
 }
 else {
-  /* read first command argument (file name) */
-  $file = (isset($argv[$argidx]) ? $argv[$argidx] : "");
-  if ($file == "")
-    $file = getenv("HOME") . "/logs/error_log";
-  // if ($file == "") {
-    // fputs(STDERR, "Usage: " . $argv[0] . " [-n <lines>] <file>\n");
-    // exit(1);
-  // }
-
-  $tail = new Tail($file);
-  $tail->rollbackBytes(10485760);
+  $tail->rollbackBytes($opt_rollback_bytes);
 }
 
 $Months = array(
@@ -118,6 +252,10 @@ $Filter = array(
 $FilterExclude = false;
 $FilterExclude = true;
 
+function parse_error_log_line($str)
+{
+}
+
 $current_date = null;
 while (($str = $tail->read()) !== false) {
   // 81.202.254.26 WonderSeller TLSv1.2 [17/May/2018:00:58:13 +0200] "GET /sell/articles/cards HTTP/1.1" 200 98356 "-" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"
@@ -129,7 +267,7 @@ while (($str = $tail->read()) !== false) {
   if (!preg_match('{^' .
         '\[(?P<datetime>[A-Za-z0-9:\. ]+)\] ' .   // [Sat May 19 17:10:13.895016 2018]
         '\[(?P<channel>\w+):(?P<level>\w+)\] ' .  // [ssl:info]
-        '\[pid (?P<pid>\d+)\] ' .                // [pid 20410]
+        '\[pid (?P<pid>\d+)(?::tid (?P<tid>\d+))?\] ' .  // [pid 20410] or [pid 30712:tid 30712]
         '(?P<prefix>[^\[]*)' .                       // (104)Connection reset by peer:
         '(?:\[client (?P<ip>[0-9a-f:\.]+)\] )?' .     // [client 66.249.66.159:58972]
         '(?P<message>.*)$}', $str, $regp)) {
@@ -189,8 +327,8 @@ while (($str = $tail->read()) !== false) {
       $filtered = true;
   }
 
-  if ($filtered === $FilterExclude)
-    continue;
+  // if ($filtered === $FilterExclude)
+    // continue;
 
   // print formatted line
   $line = sprintf("%8s %-40s %-10s %-8s %s",
